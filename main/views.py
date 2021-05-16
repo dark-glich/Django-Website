6ec301721adceefd4e50abacb97ecfaf95d7d4d0
dark-glich <mohdbilal9808464636@gmail.com>
2021-05-16 23:05:55 +0530

    completed comments

--- main/views.py
+++ main/views.py
@@ -4,13 +4,13 @@ from .forms import Comment_Section
 
 def HomeView(request):
      form = Comment_Section(request.POST or None)
-     if form.is_valid() and 'comment' in request.data:
-          name = form.cleaned_data['name'].capitalize()
-          CommentSection.objects.create(name=name, email=form.cleaned_data['email'],
-                                        comment=form.cleaned_data['comment'])
+     if form.is_valid() :
+          if '_comment' in request.POST:
+               name = form.cleaned_data['name'].capitalize()
+               CommentSection.objects.create(name=name, email=form.cleaned_data['email'],comment=form.cleaned_data['comment'])
           form = Comment_Section()
           
-     comments = CommentSection.objects.all().order_by('-date_added')
+     comments = CommentSection.objects.all().order_by('-date_added')[:15]
      count= CommentSection.objects.all().count()
      context = {
           'count':count,
